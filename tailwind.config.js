44659e860feb06fe05af79e63d8609a1be558921
Ralph Arsy Bauto <rab.devph@gmail.com>
2024-02-26 15:52:21 +0400

    Add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        opensans: ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
